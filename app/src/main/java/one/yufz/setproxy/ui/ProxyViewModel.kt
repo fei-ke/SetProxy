204aae905874bad7772ff097a45506db8cb93cad
yufz <jinyang656@gmail.com>
2023-01-06 13:23:50 +0800

    Request permission

--- app/src/main/java/one/yufz/setproxy/ui/ProxyViewModel.kt
+++ app/src/main/java/one/yufz/setproxy/ui/ProxyViewModel.kt
@@ -1,8 +1,13 @@
 package one.yufz.setproxy.ui
 
+import android.Manifest
 import android.app.Application
+import android.content.pm.PackageManager
+import android.os.Build
+import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import one.yufz.setproxy.DeviceProxyManager
 import one.yufz.setproxy.Proxy
@@ -15,6 +20,10 @@ class ProxyViewModel(private val app: Application) : AndroidViewModel(app) {
 
     val proxyList: StateFlow<List<Proxy>> = proxyStore.listFlow()
 
+    private val _requestPermission = MutableStateFlow(false)
+
+    val requestPermission: Flow<Boolean> = _requestPermission
+
     fun addProxy(proxy: Proxy) {
         proxyStore.addProxy(proxy)
     }
@@ -24,10 +33,32 @@ class ProxyViewModel(private val app: Application) : AndroidViewModel(app) {
     }
 
     fun activateProxy(proxy: Proxy) {
-        DeviceProxyManager.setProxy(app, proxy)
+        if (checkPermission()) {
+            DeviceProxyManager.setProxy(app, proxy)
+        }
     }
 
     fun deactivateProxy() {
-        DeviceProxyManager.removeProxy(app)
+        if (checkPermission()) {
+            DeviceProxyManager.removeProxy(app)
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        val granted = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            app.checkSelfPermission(Manifest.permission.WRITE_SECURE_SETTINGS)
+        } else {
+            app.checkPermission(Manifest.permission.WRITE_SECURE_SETTINGS, android.os.Process.myPid(), android.os.Process.myUid())
+        } == PackageManager.PERMISSION_GRANTED
+
+        _requestPermission.value = !granted
+
+        Log.i("PermissionRequire", "adb shell pm grant one.yufz.setproxy android.permission.WRITE_SECURE_SETTINGS")
+
+        return granted
+    }
+
+    fun cancelRequestPermission() {
+        _requestPermission.value = false
     }
 }
